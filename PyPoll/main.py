3d2108ffb6492964ac368eda9abfda8cc438b92e
Malvika <malvika.mathur@Gmail.com>
2018-03-12 23:41:56 -0400

    python challenge commits

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,40 +1,11 @@
-# In this challenge, you are tasked with helping a small, rural town modernize its vote-counting process.
-#(Up until now, Uncle Cleetus had been trustfully tallying them one-by-one, but unfortunately,
-#his concentration isn't what it used to be.)
-#
-# You will be given two sets of poll data (election_data_1.csv and election_data_2.csv).
-#Each dataset is composed of three columns: Voter ID, County, and Candidate.
-#Your task is to create a Python script that analyzes the votes and calculates each of the following:
-
-# The total number of votes cast
-
-# A complete list of candidates who received votes
-#
-# The percentage of votes each candidate won
-#
-# The total number of votes each candidate won
-#
-# The winner of the election based on popular vote.
-#
-# As an example, your analysis should look similar to the one below:
-#
-# Election Results
-# -------------------------
-# Total Votes: 620100
-# -------------------------
-# Rogers: 36.0% (223236)
-# Gomez: 54.0% (334854)
-# Brentwood: 4.0% (24804)
-# Higgins: 6.0% (37206)
-# -------------------------
-# Winner: Gomez
-# -------------------------
-# Your final script must be able to handle any such similarly-structured dataset in the future (i.e you have zero intentions of living in this hillbilly town -- so your script needs to work without massive re-writes). In addition, your final script should both print the analysis to the
-# terminal and export a text file with the results.
-
 
 import csv
 import os
+
+#outputting results to a text file called Poll results
+output_file = "Poll_Results.txt"
+   #open the output file
+file = open(output_file, 'w')
 
 candidates = []
 all_votes = []
@@ -47,10 +18,14 @@ def printResults():
     print('----------------------------')
     print('Total Votes: ', total_votes)
     print('----------------------------')
+    file.write('Election Results\n')
+    file.write('----------------------------\n')
+
 
     for can in candidates:
         candidate_count = all_votes.count(can)
         print(can + ': ' + str( round((candidate_count / total_votes) * 100, 1) ) + '% (' + str(candidate_count) + ')')
+        file.write(can + ': '  + str( round((candidate_count / total_votes) * 100, 1) ) + '% (' + str(candidate_count) + ')' + '\n')
 
         if candidate_count > winner_count:
             winner_count = candidate_count
@@ -58,11 +33,14 @@ def printResults():
 
     print('----------------------------')
     print('Winner : ' + winner)
-    print('----------------------------')
+    print('----------------------------')    
+    file.write("Total Votes: " + str(total_votes) + '\n')
+    file.write('----------------------------\n')
+    file.write('Winner : ' + winner + '\n')
+    file.write('----------------------------\n')
 
 
 csvpath = os.path.join('raw_data', 'election_data_1.csv')
-
 with open(csvpath, newline='') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
@@ -81,6 +59,5 @@ with open(csvpath, newline='') as csvfile:
 
         # add each candidate to all_votes array
         all_votes.append(rowsArray[i][2])
-
 
 printResults()
